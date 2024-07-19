1712a5099fa2e7a1a6713d92b3fec8a4598eb5b5
Yocanantham <yocanantham@gmail.com>
2024-07-19 11:26:05 +0530

    fix : the react application here

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// ! import the React and ReactDOM
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ! get the div element with id root
+
+const element = document.getElementById("root");
+
+// ! Tell the react to control the element
+
+const root = ReactDOM.createRoot(element);
+
+// ! Add the Component which will return a JSX
+
+function App() {
+    // ? Simple Message
+
+    // return <h1>First React Practice</h1>;
+
+    // ? Using a variable
+
+    // let message = 'New Message!';
+    // if (Math.random() > 1) {
+    //     message = 'Working!';
+    // }
+    // return <h1>{ message }</h1>;
+
+    // -> Varaibles should be enclosed in a curly.
+
+    // ? Shorthands variable
+
+    // const date = new Date();
+    // const time = date.toLocaleTimeString();
+    // return <h1>{time}</h1>;
+    //// Alternate form
+    // return <h1>{new Date().toLocaleTimeString()}</h1>;
+
+    // ? Exercise
+    // const name = "Developer";
+    // return (
+    //     <div>
+    //         <h1>My name is : {name}</h1>
+    //     </div>
+    // );
+}
+
+// ! Show the component
+
+root.render(<App />);
