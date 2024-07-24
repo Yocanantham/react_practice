b31c036166685de3db3abe03517b2cf84e328061
Yocanantham <yocanantham@gmail.com>
2024-07-24 15:02:55 +0530

    feat : add import / export definition

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import App from "./App";
 // ! get the div element with id root
 
 const element = document.getElementById("root");
@@ -13,37 +13,40 @@ const root = ReactDOM.createRoot(element);
 
 // ! Add the Component which will return a JSX
 
-function App() {
-    // ? Simple Message
+// function App() {
+//     // ? Simple Message
 
-    // return <h1>First React Practice</h1>;
+//     // return <h1>First React Practice</h1>;
 
-    // ? Using a variable
+//     // ? Using a variable
 
-    // let message = 'New Message!';
-    // if (Math.random() > 1) {
-    //     message = 'Working!';
-    // }
-    // return <h1>{ message }</h1>;
+//     // let message = 'New Message!';
+//     // if (Math.random() > 1) {
+//     //     message = 'Working!';
+//     // }
+//     // return <h1>{ message }</h1>;
 
-    // -> Varaibles should be enclosed in a curly.
+//     // -> Varaibles should be enclosed in a curly.
 
-    // ? Shorthands variable
+//     // ? Shorthands variable
 
-    // const date = new Date();
-    // const time = date.toLocaleTimeString();
-    // return <h1>{time}</h1>;
-    //// Alternate form
-    // return <h1>{new Date().toLocaleTimeString()}</h1>;
+//     // const date = new Date();
+//     // const time = date.toLocaleTimeString();
+//     // return <h1>{time}</h1>;
+//     //// Alternate form
+//     // return <h1>{new Date().toLocaleTimeString()}</h1>;
 
-    // ? Exercise
-    // const name = "Developer";
-    // return (
-    //     <div>
-    //         <h1>My name is : {name}</h1>
-    //     </div>
-    // );
-}
+//     // ? Exercise
+//     // const name = "Developer";
+//     // return (
+//     //     <div>
+//     //         <h1>My name is : {name}</h1>
+//     //     </div>
+//     // );
+
+
+    
+// }
 
 // ! Show the component
 
